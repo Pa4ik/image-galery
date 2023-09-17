dda82447e4cb77e62c18f82f443c0c40f67f0eb3
Maxim Pachuzheuski <makspohp123@gmail.com>
2023-09-17 10:17:45 +0300

    feat:added animation for pause and play

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -1,1 +1,16 @@
-console.log('0/60')
+console.log('10/60');
+
+// переключение кнопки плей и паузы 
+let getImg = document.getElementsByClassName('pl-pa');    
+for (let i = 0; i < getImg.length; i++) {
+    getImg[i].onclick = function (objTarget) {
+        let imgTarget = objTarget.target;
+        let imgClick = imgTarget.getAttribute('data-replace');
+        let imgSrc = imgTarget.src;
+
+        imgTarget.setAttribute('data-replace',imgSrc);
+        imgTarget.setAttribute('src',imgClick);    
+    }
+}
+
+
