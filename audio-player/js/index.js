0cd52d2cc158ef418b2a6f0836faf24802327c87
Maxim Pachuzheuski <makspohp123@gmail.com>
2023-09-17 17:35:53 +0300

    feat:Added function to turn song and cover animation on and off

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -14,3 +14,65 @@ for (let i = 0; i < getImg.length; i++) {
 }
 
 
+
+
+const player = document.querySelector('.main-container')
+const playBtn = document.querySelector('.pl-pa')
+const pverBtn = document.getElementById('prevSong')
+const nextBtn = document.getElementById('nextSong')
+const currentTime = document.querySelector('.currentTime')
+const durationTime = document.querySelector('.durationTime')
+const progressBar = document.getElementById('song-length')
+const songExecutor = document.querySelector('.song-executor')
+const songName = document.querySelector('.song-name')
+const cover = document.getElementById('cover')
+const audio = document.getElementById('audio')
+const backImg = document.querySelector('.back-img')
+
+
+
+// название песен 
+const songs = ['Don t Hurt Yourself', 'Don t Start Now'];
+//артисты
+const artist = ['Beyonce', 'Dua Lipa']
+
+// первая песня 
+let songIndex = 1;
+ 
+
+// init
+function loadSong(song){
+    songName.innerHTML = song
+    audio.src = `assets/audio/${song}.mp3`
+    cover.src = `assets/img/cover${songIndex + 1}.png`
+    backImg.src = `assets/img/cover${songIndex + 1}.png`
+    songExecutor.innerHTML = artist[songIndex]
+}
+
+loadSong(songs[songIndex])
+
+
+//Play
+function playSong () {
+    player.classList.add('play')
+    cover.style.transform =' scale(1.1)';
+    audio.play()
+}
+
+//Pause
+function pauseSong () {
+    player.classList.remove('play')
+    cover.style.transform = 'scale(1)';
+    audio.pause()
+}
+
+//play-pause
+playBtn.addEventListener('click', () => {
+    const isPlaying = player.classList.contains('play')
+    if(isPlaying){
+        pauseSong()
+    } else {
+        playSong()
+    }
+})
+
